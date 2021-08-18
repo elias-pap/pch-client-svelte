697f1f7e7cc5ea5331e9f062b4fe0dd6447302fd
Elias Papavasileiou <eliaspap@codebgp.com>
2021-08-18 18:43:55 +0300

    import react components, began work on router

---
+++ src/components/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Router, Route } from 'svelte-routing';
+	import { Dashboard } from '../pages/Dashboard.svelte';
+import { Auth } from '../pages/Auth.svelte';
+</script>
+
+<main>
+  <Router>
+    <Route path="/dashboard">
+      <Dashboard {isLoggedIn} {updateLoggedIn} />
+    </Route>
+    <Route path="/auth">
+      <Auth {isLoggedIn} {updateLoggedIn} />
+    </Route>
+    <Route path="/">
+      <Redirect to="/auth" />;
+    </Route>``
+  </Router>
+</main>
